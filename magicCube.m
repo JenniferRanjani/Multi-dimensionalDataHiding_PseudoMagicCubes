deb6e1c6d91ba0455c04e54d07d945c0f3e192cb
JenniferRanjani <43373794+JenniferRanjani@users.noreply.github.com>
2020-12-04 11:02:57 +0530

    Update magicCube.m

--- magicCube.m
+++ magicCube.m
@@ -11,7 +11,7 @@ for i = 1:N
             b = mod(i+rx-j-ry-k-rz,N);
             c = mod(i+rx+j+ry+k+rz-2,N);
             
-            MS(i,j,k) = a*N^2 + b*N+ c+1;
+            MS(i,j,k) = a*N^2 + b*N+ c;
             
         end
     end
